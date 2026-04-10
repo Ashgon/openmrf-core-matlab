29badb04210389d663967670d3588e76b26d4706
UNI-WUERZBURG\mag28ev <maximilian.gram@uni-wuerzburg.de>
2026-04-10 21:48:05 +0200

    correct input arguments for TRAJ reco example

--- main_sequences/calibrate_trajectories/reco_traj_meas.m
+++ main_sequences/calibrate_trajectories/reco_traj_meas.m
@@ -1,7 +1,8 @@
 %% get measured trajectory
 clear
 
+vendor     = 'Siemens';
 study_path = 'Q:/data/Pulseq/Rawdata/mgram/Skyra/250716_strukturphantom_git_test/';
 study_name = 'meas_MID00736_FID228644_pulseq_traj_cmrf.dat';
 
-[ktraj_meas, ktraj_hash] = TRAJ_reco([study_path study_name], [8:8:48]);
+[ktraj_meas, ktraj_hash] = TRAJ_reco([study_path study_name], [], vendor, [8:8:48]);
